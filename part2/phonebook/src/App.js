bd933451865f97cfead9b26ef53ca1180c6975e8
jonkur <jonkur@users.noreply.github.com>
2020-07-22 16:40:14 +0300

    Complete exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import NameFilter from './components/NameFilter'
 import NewContactForm from './components/NewContactForm'
 import ContactList from './components/ContactList'
@@ -10,6 +11,14 @@ const App = () => {
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ nameFilter, setNameFilter ] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const handleSubmit = (event) => {
     event.preventDefault()
