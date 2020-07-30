64a6c99d5a53bcb63971edae30c048e69f1cfdf8
jonkur <jonkur@users.noreply.github.com>
2020-07-30 19:04:44 +0300

    Complete exercises 2.19-2.20

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import NameFilter from './components/NameFilter'
 import NewPersonForm from './components/NewPersonForm'
 import PersonList from './components/PersonList'
+import Notification from './components/Notification'
 import personService from './services/persons'
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ nameFilter, setNameFilter ] = useState('')
+  const [ notificationMsg, setNotificationMsg ] = useState('')
 
   useEffect(() => {
     personService.getAll()
@@ -23,14 +25,32 @@ const App = () => {
       if (window.confirm(`Person ${newName} is already in the list. Do you want to replace the old number with this one?`)) {
         const personToUpdate = persons.find(person => person.name.toLowerCase() === newName.toLowerCase())
         personService.update({...personToUpdate, number: newNumber})
-          .then(updatedPerson => setPersons(persons.map(p => p.id !== updatedPerson.id ? p : updatedPerson)))
+          .then(updatedPerson => {
+            setPersons(persons.map(p => p.id !== updatedPerson.id ? p : updatedPerson))
+            setNotification(`Number for ${updatedPerson.name} was changed.`, false, 2000)
+          }).catch(e => {
+            setPersons(persons.filter(p => p.id !== personToUpdate.id))
+            setNotification(`Entry for ${personToUpdate.name} has been removed, cannot update.`, true, 3000)
+          })
+          setNewName('')
+          setNewNumber('')
       }
       return
     }
     personService.create({name: newName, number: newNumber})
-      .then(addedPerson => setPersons(persons.concat(addedPerson)))
+      .then(addedPerson => {
+        setPersons(persons.concat(addedPerson))
+        setNotification(`Added ${addedPerson.name}.`, false, 2000)
+      })
     setNewName('')
     setNewNumber('')
+  }
+
+  const setNotification = (message, errorState, time) => {
+    setNotificationMsg({msg: message, errorState: errorState})
+    setTimeout(() => {
+      setNotificationMsg('')
+    }, time)
   }
 
   const handleNewName = (event) => {
@@ -50,13 +70,17 @@ const App = () => {
       return
     }
     personService.remove(person)
-      .then(person => setPersons(persons.filter(p => p.id !== person.id)))
+      .then(person => {
+        setPersons(persons.filter(p => p.id !== person.id))
+        setNotification(`${person.name} was removed.`, false, 2000)
+      })
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
       <NameFilter nameFilter={nameFilter} handleNameFilter={handleNameFilter} />
+      <Notification message={notificationMsg.msg} isError={notificationMsg.errorState} />
       <NewPersonForm newName={newName}
                      newNumber={newNumber}
                      handleSubmit={handleSubmit}
--- part2/phonebook/src/components/Person.js
+++ part2/phonebook/src/components/Person.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Contact = ({person, deleteHandler}) => (
+const Person = ({person, deleteHandler}) => (
     <div>
       <p>{person.name}, phone: {person.number} <button onClick={() => deleteHandler(person)}>delete</button></p>
     </div>
 )
 
-export default Contact
+export default Person
